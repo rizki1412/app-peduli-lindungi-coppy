0912adecacc3afe616df24fb0c2bba090c96179a
Muhammad Rizki <pohongw@gmail.com>
2022-10-13 19:02:18 +0700

    menambahkan loginscreen

--- App/Pages/RegistrationScreen/SignupScreen.js
+++ App/Pages/RegistrationScreen/SignupScreen.js
@@ -1,64 +1,128 @@
-import * as React from 'react';
+import * as React from "react";
 import {
-    View, 
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TextInput
-} from 'react-native';
-import { PrimaryButton  } from '../../Componets'
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TextInput,
+} from "react-native";
+import {
+  LogoBumn,
+  LogoKemenkes,
+  LogoKominfo,
+  LogoKpc,
+  LogoPeduli,
+  LogoPesan,
+} from "../../assets";
+import { PrimaryButton } from "../../Componets";
 
-export default SignupScreen = ({navigation}) => {
+const windowHeight = Dimensions.get("window").height;
+
+export default SignupScreen = ({ navigation }) => {
   return (
-    <SafeAreaView  style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.headerText}>Email</Text>
-        <Text style={styles.boddyText}>Enter your email adress</Text>
-        <TextInput style={styles.inputText} placeholder='youremail@domain.com'/>
+    <View style={styles.bgMainApp}>
+      <View style={styles.imgContainer}>
+        <Image source={LogoPeduli} style={styles.imgStyle} />
       </View>
-     
-     <PrimaryButton title='Next' style={styles.customeButton}  onPress={()=>navigation.navigate('PersonalInfoScreen')}/>
-     <View style={styles.foother}>
-       <Text style={styles.boddyText}>Already have an account? </Text>
-       <Text style={[styles.boddyText,{color:'#006175'}]}>Sign In</Text>
-     </View>
-    </SafeAreaView>
+      <View>
+        <TextInput
+          Image
+          source={LogoPesan}
+          style={styles.Input}
+          placeholder="youremail@domain.com"
+        />
+        <TextInput
+          style={styles.InputPass}
+          placeholder="Masukkan Password Anda"
+        />
+        <PrimaryButton
+          title="Login"
+          style={styles.customeButton}
+          onPress={() => navigation.navigate("HomeScreen")}
+        />
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.footherText}>Bekerjasama Dengan :</Text>
+      </View>
+      <View style={styles.imgStyle2}>
+        <Image source={LogoKpc} style={styles.imgSize} />
+        <Image source={LogoKominfo} style={styles.imgSize} />
+        <Image source={LogoKemenkes} style={styles.imgSize} />
+        <Image source={LogoBumn} style={styles.imgSize} />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container:{
-       flex: 1, 
-       paddingLeft:30,
-       paddingRight:25,
-    },
-    content:{
-      marginTop:200
-    },
-    headerText:{
-      fontFamily:'Poppins-SemiBold',
-      fontSize:20
-    },
-    boddyText:{
-      fontFamily:'Poppins-Medium',
-      fontSize:16,
-    },
-    inputText:{
-      borderColor:'#006175',
-      fontSize:16,
-      paddingTop:6,
-      paddingBottom:6,
-      paddingRight:6,
-      paddingLeft:10,
-      borderWidth:1,
-      borderRadius:10,
-    },
-    foother:{
-      justifyContent:'center',
-      flexDirection:'row',
-      marginTop:20
-    },
-    customeButton:{
-      marginTop:250
-    }
+  bgMainApp: {
+    flex: 1,
+  },
+  imgContainer: {
+    alignItems: "center",
+  },
+  container: {
+    margin: 40,
+  },
+  imgStyle: {
+    marginTop: windowHeight / 8,
+    marginBottom: 20,
+  },
+  Input: {
+    borderWidth: 1,
+    borderColor: "#0f663f",
+    padding: 7,
+    marginStart: 20,
+    fontSize: 17,
+    marginEnd: 20,
+    borderRadius: 10,
+    padding: 10,
+    fontWeight: "bold",
+  },
+  InputPass: {
+    borderWidth: 1,
+    borderColor: "#0f663f",
+    padding: 7,
+    marginStart: 20,
+    fontSize: 17,
+    marginTop: 20,
+    marginEnd: 20,
+    borderRadius: 10,
+    padding: 10,
+    fontWeight: "bold",
+  },
+  imgStyle2: {
+    flexDirection: "row",
+    marginRight: 30,
+    marginLeft: 40,
+  },
+  imgSize: {
+    width: null,
+    resizeMode: "contain",
+    height: 50,
+    margin: 0,
+    flex: 1,
+  },
+  headerText: {
+    textAlign: "left",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  bodyText: {
+    marginTop: 10,
+    marginBottom: 50,
+    fontSize: 16,
+  },
+  footherText: {
+    marginTop: 100,
+    fontSize: 16,
+    marginBottom: 0,
+  },
+  customeButton: {
+    marginTop: 30,
+    marginRight: 30,
+    marginLeft: 30,
+  },
 });
