6eab09c211ba9a6981f3d26aab90ce02b6840a4f
Muhammad Rizki <pohongw@gmail.com>
2022-10-13 18:52:53 +0700

    menambahkan routes

--- App/Routes.js
+++ App/Routes.js
@@ -4,10 +4,9 @@ import {
   HomeScreen,
   SplashScreen,
   SignupScreen,
-  VerificationScreen,
-  PersonalInfoScreen,
-  RegisterDoneScreen,
-  SecurityScreen,
+  CovidScreen,
+  ImunizationScreen,
+  EhacScreen,
 } from "./Pages";
 
 const Stack = createStackNavigator();
@@ -16,9 +15,34 @@ export default Router = () => {
   return (
     <Stack.Navigator initialRouteName="SplashScreen">
       <Stack.Screen
+        name="SignupScreen"
+        component={SignupScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
         name="SplashScreen"
         component={SplashScreen}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="CovidScreen"
+        component={CovidScreen}
+        options={{ headerShown: true, title: "COVID - 19 Test Results" }}
+      />
+      <Stack.Screen
+        name="ImunizationScreen"
+        component={ImunizationScreen}
+        options={{ headerShown: true, title: "Vaccine and Immunization" }}
+      />
+      <Stack.Screen
+        name="EhacScreen"
+        component={EhacScreen}
+        options={{ headerShown: true, title: "Ehac", headerTransparent: true }}
       />
     </Stack.Navigator>
   );
